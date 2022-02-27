f899dfb114869e36d977000fa1f603c0d4f81bbe
MrFarhanKhan007 <96666465+MrFarhanKhan007@users.noreply.github.com>
2022-02-28 01:40:11 +0530

    Create LinkedListUse5.java

---
+++ Data Structures in JAVA/Level 2/Lecture 8: Linked List 1/Linked List classwork files/LinkedListUse5.java
@@ -0,0 +1,30 @@
+package LinkedLists;
+public  class LinkedListUse5{
+
+    public static void print(Node<Integer> head){
+        Node<Integer> temp = head;
+
+        while(temp != null){
+            System.out.print(temp.data +" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void increment(Node<Integer> head){
+        Node<Integer> temp = head;
+        while(temp != null){
+            temp.data++;
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String args[]){
+
+        Node<Integer> node1 = new Node<Integer>(10);
+        Node<Integer> node2 = new Node<Integer>(20);
+        node1.next = node2;
+        increment(node1);
+        print(node1);
+    }
+}
