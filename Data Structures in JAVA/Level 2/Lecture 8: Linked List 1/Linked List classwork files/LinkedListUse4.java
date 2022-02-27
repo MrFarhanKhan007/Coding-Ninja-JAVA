b3b873b8765ec830a592968b7871c90bdb2ba2e2
MrFarhanKhan007 <96666465+MrFarhanKhan007@users.noreply.github.com>
2022-02-28 01:39:57 +0530

    Create LinkedListUse4.java

---
+++ Data Structures in JAVA/Level 2/Lecture 8: Linked List 1/Linked List classwork files/LinkedListUse4.java
@@ -0,0 +1,26 @@
+package LinkedLists;
+
+public class LinkedListUse4{
+
+    public static void print(Node<Integer> head){
+        Node<Integer> temp = head;
+
+        while(temp != null){
+            System.out.print(temp.data +" ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+
+        Node<Integer> node1 = new Node<Integer>(10);
+        Node<Integer> node2 = new Node<Integer>(20);
+        Node<Integer> node3 = new Node<Integer>(30);
+        Node<Integer> node4 = new Node<Integer>(40);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        print(node2);
+    }
+}
