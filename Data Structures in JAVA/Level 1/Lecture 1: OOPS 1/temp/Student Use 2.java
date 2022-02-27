cd6cee5a5bbc43b1ee8c93ca22104d213a60314b
MrFarhanKhan007 <96666465+MrFarhanKhan007@users.noreply.github.com>
2022-02-28 01:03:43 +0530

    Create Student Use 2.java

---
+++ Data Structures in JAVA/Level 1/Lecture 1: OOPS 1/temp/Student Use 2.java
@@ -0,0 +1,12 @@
+package temp;
+
+import classes_and_objects.Student;
+
+public class StudentUse2 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Student s = new Student();
+
+    }
+}
