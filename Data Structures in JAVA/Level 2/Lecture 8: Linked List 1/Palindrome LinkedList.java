da0b636a4be2e1a668a629b2d42b6f2c0e6b7c48
MrFarhanKhan007 <96666465+MrFarhanKhan007@users.noreply.github.com>
2022-02-28 01:35:41 +0530

    Update Palindrome LinkedList.java

--- Data Structures in JAVA/Level 2/Lecture 8: Linked List 1/Palindrome LinkedList.java
+++ Data Structures in JAVA/Level 2/Lecture 8: Linked List 1/Palindrome LinkedList.java
@@ -1,83 +1,74 @@
-// Delete Node in LL
+// Palindrome LinkedList
 // Send Feedback
-// Given a linked list and a position i, delete the node of ith position from Linked List iteratively.
-// If position i is greater than length of LL, then you should return the same LL without any change.
-// Indexing starts from 0. You don't need to print the elements, just delete the node and return the head of updated LL.
-// Input format :
-// Line 1 : Linked list elements (separated by space and terminated by -1)
-// Line 2 : Integer i (position)
-// Output format :
-// Updated LL elements (separated by space)
+// Check if a given linked list is palindrome or not. Return true or false.
+// Indexing starts from 0.
+// Input format : Linked list elements (separated by space and terminated by -1)
+
 // Sample Input 1 :
-// 3 4 5 2 6 1 9 -1
-// 3
+// 9 2 3 3 2 9 -1
 // Sample Output 1 :
-// 3 4 5 6 1 9
+// true
 // Sample Input 2 :
-// 3 4 5 2 6 1 9 -1
-// 0
+// 0 2 3 2 5 -1
 // Sample Output 2 :
-// 4 5 2 6 1 9
-
-/*************** 
- * Following is the Node class already written 
+// false
+import java.util.*;
+/*
 class LinkedListNode<T> {
-	T data;
-	LinkedListNode<T> next;
+	public T data;
+	public LinkedListNode<T> next;
 
-	public Node(T data) {
+	public LinkedListNode(T data) {
+		this.setData(data);
+		this.next = null;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
 		this.data = data;
 	}
+
 }
- ***************/
-
+* */
 public class Solution {
-
-	public static LinkedListNode<Integer> deleteIthNode(LinkedListNode<Integer> head, int i){
-		/* Your class should be named Solution
-		 * Don't write main().
-		 * Don't read input, it is passed as function argument.
-		 * Return output and don't print it.
-	 	 * Taking input and printing output is handled automatically.
-		*/LinkedListNode<Integer> temp=head;
-        int counter=0;
-        while(head!=null)
-        {head=head.next;
-        counter++;}
-        
-        head=temp;
-        
-        //if element present at Zeroth index
-        
-        if(i==0){
-            head=head.next;
-        //return head;
-        }
-        
-        //if element is present inbetween
-        if(i<counter)
-        { 
-           while(i>1){
-               head=head.next;
-            	i--;
-           }
-            
-            head.next=head.next.next;
-            head=temp;
-            //return head;
-            
-        }
-         //if element is present at last
-        if(i==counter){
-            while(i>1){
-               head=head.next;
-            	i--;
-           }
-            head.next=null;
-             head=temp;
-        }
-		
-        return head;
-        
+     public static int LengthIterative(LinkedListNode<Integer> head){
+		int count=0;
+		while(head!=null)
+		{
+			count++;
+			head=head.next;
+		}
+		return count;
 	}
+    
+    
+	public static boolean isPalindrome(LinkedListNode<Integer> root) {
+        
+         int length=LengthIterative(root);
+        ArrayList<Integer> list1=new ArrayList<Integer>(length);
+        for(int i=0;i<length;i++)
+        {
+            list1.add(root.data);
+            root=root.next;
+        }
+        
+        int tail=length-1;
+         int i=0;
+        boolean isPalindrome=true;
+        while(i<=tail)
+        {
+            if(list1.get(i)!=list1.get(tail))
+            {
+                isPalindrome=false;
+            	return false;
+            }
+            i++;
+            tail--;
+        }
+        
+        return isPalindrome;
+    }
 }
