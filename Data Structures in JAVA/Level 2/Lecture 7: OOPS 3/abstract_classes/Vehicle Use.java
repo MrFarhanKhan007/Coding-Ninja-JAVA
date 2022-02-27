1cdf12fb4f7a7262a676d8b58f5ecc6fa0d05cf1
MrFarhanKhan007 <96666465+MrFarhanKhan007@users.noreply.github.com>
2022-02-28 01:31:23 +0530

    Create Vehicle Use.java

---
+++ Data Structures in JAVA/Level 2/Lecture 7: OOPS 3/abstract_classes/Vehicle Use.java
@@ -0,0 +1,43 @@
+package abstract_classes;
+
+import java.util.Scanner;
+
+public class VehicleUse {
+
+    public static void main(String[] args) {
+        //Vehicle v = new Vehicle(20);
+        Vehicle v = new Car(1, 2) {
+            @Override
+            public String getCompany() {
+                return null;
+            }
+        };
+
+//		v.color = "Black";
+//		v.setMaxSpeed(10);
+        //v.print();
+
+        int n;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+
+        if (n == 0) {
+            v = new Car(10, 20) {
+                @Override
+                public String getCompany() {
+                    return null;
+                }
+            };
+        } else {
+            //v = new Vehicle(20);
+        }
+        //v.isConvertible();
+        //v.print();
+
+//		Car c = new Car(10,20);
+//		c.color = "Black";
+//		c.setMaxSpeed(100);
+        //c.print();
+    }
+
+}
