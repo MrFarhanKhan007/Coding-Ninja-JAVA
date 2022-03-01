468e41bbf177b575b3ba7eb233d081fe69894be7
MrFarhanKhan007 <96666465+MrFarhanKhan007@users.noreply.github.com>
2022-03-01 18:53:37 +0530

    Create Code : Midpoint of Linked list.java

---
+++ Data Structures in JAVA/Level 2/Lecture 9: Linked List 2/Code : Midpoint of Linked list.java
@@ -0,0 +1,19 @@
+
+public class Solution {
+    
+    public static LinkedListNode<Integer> midPoint(LinkedListNode<Integer> head) {
+        //Your code goes here
+        if (head==null || head.next==null)
+        {
+            return head;
+        }
+        LinkedListNode<Integer> fast=head,slow=head;
+        while(fast.next!=null && fast.next.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+
+}
