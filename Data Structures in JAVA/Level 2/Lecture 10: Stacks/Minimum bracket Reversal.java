ca8e654459451aa19e7c9176e73b16b8fe30c0dc
MrFarhanKhan007 <96666465+MrFarhanKhan007@users.noreply.github.com>
2022-03-06 13:41:43 +0530

    Update Minimum bracket Reversal.java

--- Data Structures in JAVA/Level 2/Lecture 10: Stacks/Minimum bracket Reversal.java
+++ Data Structures in JAVA/Level 2/Lecture 10: Stacks/Minimum bracket Reversal.java
@@ -1,38 +1,52 @@
+
 import java.util.Stack;
-public class Solution {
-	
-    public static int[] stockSpan(int[] price) {
-        // Write your code here
-        int[] span = new int[price.length];
-        Stack<Integer> stack = new Stack<Integer>();
-        span[0] = 1;
-        stack.push(0);
-
-        for(int i=1;i<price.length;i++){
-
-
-
-            while( !stack.isEmpty() && price[i] > price[ stack.peek()] ){
-                stack.pop();
+public class Solution 
+{
+    // Function returns -1 if brackets can't be balanced
+    public static int countBracketReversals(String input)
+    {
+        if(input.length()%2!=0)
+            return -1;
+        Stack<Character> stk=new Stack<>();
+        for(int i=0;i<input.length();i++)
+        {
+            if(input.charAt(i)=='{')
+            {
+                stk.push('{');
             }
-            if(stack.isEmpty()){
-                span[i] = i+1;
-               
-            }
-            else{
-                span[i] = i - stack.peek();
-                
-                
-            }
-            stack.push(i);
+            else if(input.charAt(i)=='}')
+            {
+                if(stk.isEmpty() || stk.peek()=='}')
+                {
+                    stk.push('}');
+                }
+                else if(stk.peek()=='{')
+                {
+                    stk.pop();
+                }
 
+            }}
+            int count=0;
+            while(stk.size()!=0){
+                char c1=stk.pop();
+                char c2=stk.pop(); 
 
+                if(c1==c2)
+                {
+                    if(c2=='{')
+                        c2='}';
+                    if(c1=='}')
+                        c1='{';
+                    count++;
+                }
 
-        }
-        return span;
-
-
-
+                else{
+                    c1='{';
+                    c2='}';
+                    count++;
+                    count++;
+                }}
+            return count;
+        
     }
-
 }
