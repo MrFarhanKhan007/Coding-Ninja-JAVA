109a3a97953af3b59ae3e4f886c6e2e54ad0b751
Farhan Khan <96666465+MrFarhanKhan007@users.noreply.github.com>
2022-05-06 01:10:12 +0530

    Create Subset Sum.java

---
+++ Data Structures in JAVA/Level 4/Lecture 21: DP - 1/Subset Sum.java
@@ -0,0 +1,56 @@
+/*You are given a set of N integers. You have to return true if there exists a subset that sum up to K, otherwise return false.
+Input Format
+The first line of the test case contains an integer 'N' representing the total elements in the set.
+
+The second line of the input contains N integers separated by a single space.    
+
+The third line of the input contains a single integer, denoting the value of K.
+Output Format
+Output Yes if there exists a subset whose sum is k, else output No.
+Constraints :
+1 <= N <= 10^3
+1 <= a[i] <= 10^3, where a[i] denotes an element of the set 
+1 <= K <= 10^3
+
+Time Limit: 1 sec
+Sample Input 1 :
+4
+4 3 5 2
+13
+Sample Output 1 :
+No
+Sample Input 2 :
+5
+4 2 5 6 7
+14
+Sample Output 2 :
+Yes*/
+public class Solution {
+	
+	public static boolean isSubsetPresent(int arr[],int n,int sum)
+	{
+		 
+		boolean dp[][]=new boolean[n+1][sum+1];
+	
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(i==0){
+                    dp[i][j]=false;
+                }
+                else if(j==0){
+                    dp[i][j]=true;
+                }
+            else if(arr[i-1]<=j)
+				{
+					dp[i][j]=(dp[i-1][j-arr[i-1]]||dp[i-1][j]);
+				}
+				else 
+				{
+					dp[i][j]=dp[i-1][j];
+				}
+	
+            }
+		
+            }       
+return dp[n][sum];
+    }}
