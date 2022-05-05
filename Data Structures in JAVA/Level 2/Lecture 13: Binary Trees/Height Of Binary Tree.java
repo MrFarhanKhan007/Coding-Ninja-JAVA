1da90693199570734dfa92af0f4e2d0e318a6997
Farhan Khan <96666465+MrFarhanKhan007@users.noreply.github.com>
2022-05-05 23:32:37 +0530

    Create Height Of Binary Tree.java

---
+++ Data Structures in JAVA/Level 2/Lecture 13: Binary Trees/Height Of Binary Tree.java
@@ -0,0 +1,45 @@
+/* For a given Binary Tree of integers, find and return the height of the tree.
+Example:
+                        10
+                      /      \
+                    20      30
+                   /    \
+                 40    50
+
+Height of the given tree is 3. 
+
+Height is defined as the total number of nodes along the longest path from the root to any of the leaf node.
+ Input Format:
+The first and the only line of input will contain the node data, all separated by a single space. Since -1 is used as an indication whether the left or right node data exist for root, it will not be a part of the node data.
+Output Format:
+The first and the only line of output prints the height of the given binary tree.
+Note:
+You are not required to print anything explicitly. It has already been taken care of.
+Constraints:
+0 <= N <= 10^5
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec
+Sample Input 1:
+10 20 30 40 50 -1 -1 -1 -1 -1 -1
+Sample Output 1:
+3
+Sample Input 2:
+3 -1 -1
+Sample Output 2:
+1*/
+
+public class Solution {
+
+	public static int height(BinaryTreeNode<Integer> root) {
+		if(root==null)
+        {
+            return 0;
+        }
+        int LeftHeight = height(root.left);
+        int RightHeight = height(root.right);
+        
+        return 1 + Math.max(LeftHeight,RightHeight);
+	}
+
+}
