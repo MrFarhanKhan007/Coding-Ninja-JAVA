4c01ac80b30e65c6e452a09c598934728b0e71a7
Farhan Khan <96666465+MrFarhanKhan007@users.noreply.github.com>
2022-05-06 00:31:46 +0530

    Create Search word in Trie.java

---
+++ Data Structures in JAVA/Level 4/Lecture 20: Tries and Huffman Coding/Search word in Trie.java
@@ -0,0 +1,66 @@
+/*Implement the function SearchWord for the Trie class.
+For a Trie, write the function for searching a word. Return true if the word is found successfully, otherwise return false.
+Note : main function is given for your reference which we are using internally to test the code.
+*/
+class TrieNode{
+
+	char data;
+	boolean isTerminating;
+	TrieNode children[];
+	int childCount;
+
+	public TrieNode(char data) {
+		this.data = data;
+		isTerminating = false;
+		children = new TrieNode[26];
+		childCount = 0;
+	}
+}
+
+
+public class Trie {
+
+	private TrieNode root;
+	public int count;
+	public Trie() {
+		root = new TrieNode('\0');
+	}
+
+    private boolean search(TrieNode root,String word){
+        if(word.length()==0){
+                return   root.isTerminating;
+     
+        }
+        int childindex=word.charAt(0)-'a';
+        TrieNode child=root.children[childindex];
+        if(child==null){
+            return false;
+        }
+        return search(child,word.substring(1));
+    }
+	public boolean search(String word){
+		// Write your code here
+        return search(root,word);
+	}
+
+
+	private void add(TrieNode root, String word){
+		if(word.length() == 0){
+			root.isTerminating = true;
+			return;
+		}		
+		int childIndex = word.charAt(0) - 'a';
+		TrieNode child = root.children[childIndex];
+		if(child == null){
+			child = new TrieNode(word.charAt(0));
+			root.children[childIndex] = child;
+			root.childCount++;
+		}
+		add(child, word.substring(1));
+
+	}
+
+	public void add(String word){
+		add(root, word);
+	}
+}
