94a7776d9daa9c484e99abb384db69376fdddf80
Farhan Khan <96666465+MrFarhanKhan007@users.noreply.github.com>
2022-06-21 17:55:03 +0530

    Create Rectangular numbers.java

---
+++ Java Skill Tests/Group-1/Java Test 1 : Set 4/Rectangular numbers.java
@@ -0,0 +1,35 @@
+
+public class solution {
+
+	public static void print(int n) {
+	
+        int size = 2 * n - 1; 
+        int front = 0;
+        int back = size - 1;
+        int a[][] = new int[size][size];
+        while (n != 0) {
+            for (int i = front; i <= back; i++) {
+                for (int j = front; j <= back;
+                                        j++) {
+                    if (i == front || i == back ||
+                         j == front || j == back)
+                        a[i][j] = n;
+                }
+            }
+            ++front;
+            --back;
+            --n;
+        }
+        printfn(a, size);
+	}
+     public static void printfn(int a[][], int size)
+    {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(a[i][j]);
+            }
+            System.out.println();
+        }
+
+}
+}
